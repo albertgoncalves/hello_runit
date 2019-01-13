b8cd4d0f896e154b1c7ba288117761c27120bd7f
albertgoncalves <goncalves.albert@gmail.com>
2019-01-13 00:00:14 -0500

    Linting here too.

--- shell.nix
+++ shell.nix
@@ -3,11 +3,20 @@ with pkgs; mkShell {
     name = "RUnit";
     buildInputs = [ R
                     rPackages.RUnit
+                    rPackages.lintr
+                    gawk
                   ];
     shellHook = ''
         if [ $(uname -s) = "Darwin" ]; then
             alias ls='ls --color=auto'
             alias ll='ls -al'
         fi
+
+        lintr() {
+            R -e "library(lintr); lint('$1')" | \
+                awk '/> /{ found=1 } { if (found) print }'
+        }
+
+        export -f lintr
     '';
 }
